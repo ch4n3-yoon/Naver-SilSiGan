ebcb7b586865fd8280c7f6dc52791ae6a0666e90
SeokChan-Yoon <chaneyoon@gmail.com>
2016-11-16 13:14:07 +0900

    Create CreateDB.py
    DB 생성하는 파이썬 프로그램

---
+++ CreateDB.py
@@ -0,0 +1,16 @@
+# -*- coding : UTF-8 -*-
+import sqlite3
+
+# DB 생성 및 sqlite3의 커넥트, 커서 객체 생성
+conn = sqlite3.connect("naverSilSiGan.db")
+cursor = conn.cursor()
+
+# sqlite 쿼리 작성
+query = """CREATE TABLE `naver` (
+	      `search`	TEXT NOT NULL,
+	      `no`	INTEGER NOT NULL,
+	      PRIMARY KEY(`no`)
+          );"""
+
+# 쿼리 실행
+cursor.execute(query)
